5bd04a960860b2c7169a9f9c6fb363ca9140bc27
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-25 11:32:18 +0100

    removed parts i dont need

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,8 @@
 function Message() {
-    return <h1>Testing, hello world</h1>
+    const test = 'another test'
+    if (test)
+        return <h1>Testing, {test}</h1>
+    return <h1>Nothing</h1>
 }
+
+export default Message;
