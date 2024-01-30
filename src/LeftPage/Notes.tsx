ee87aacdb74830bad7f6610e7313667808435907
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-30 12:10:36 +0100

    placeholder notes tab
    added placeholders to the "notes" tab to show what the finished product might look like

--- src/LeftPage/Notes.tsx
+++ src/LeftPage/Notes.tsx
@@ -1,8 +1,20 @@
 function Notes() {
     return (
         <div>
+            <h2><span className="font-bold">Active Quests:</span> </h2>
+            <ul>
+                <li>Find the necromancer's tablet</li>
+                <li>Help the lost dog</li>
+                <li>Kill the Lich</li>
+            </ul>
+            <hr />
+            <br />
+            <h2><span className="font-bold">New Characters:</span> </h2>
+            <textarea className="w-full">
+                Had to improvise a npc character called "thom", he is a grumpy farmhand with a slouch. Need to add him to the npc list after session.
+            </textarea>
             <h2><span className="font-bold">Notes:</span> </h2>
-            <textarea className="h-screen w-full">
+            <textarea className="w-full">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
