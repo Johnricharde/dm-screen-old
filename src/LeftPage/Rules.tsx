bb3bb628ca469754069693db9c16a353f07332d3
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-30 12:27:43 +0100

    placeholder for "rules" and "spells" tab
    adder placeholders for the rules tab and the spells tab to show what it might look like later.

--- src/LeftPage/Rules.tsx
+++ src/LeftPage/Rules.tsx
@@ -4,11 +4,22 @@ function Rules() {
             <input type="text" placeholder="Search.."></input>
             <hr />
             <h2><span className="font-bold">Rules:</span> </h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <h1 className="text-2xl font-bold">Petrified</h1>
+            <ul className="list-disc list-inside">
+                <li>A petrified creature is transformed, along with any
+                    nonmagical object it is wearing or carrying, into a
+                    solid inanimate substance (usually stone). Its weight
+                    increases by a factor of ten, and it ceases aging.</li>
+                <li>The creature is incapacitated (see the condition), can't
+                    move or speak, and is unaware of its surroundings.</li>
+                <li>Attack rolls against the creature have advantage.</li>
+                <li>The creature automatically fails Strength and Dexterity
+                    saving throws.</li>
+                <li>The creature has resistance to all damage.</li>
+                <li>The creature is immune to poison and disease,
+                    although a poison or disease already in its system
+                    is suspended, not neutralized.</li>
+            </ul>
             <hr />
         </div>
     )
--- src/LeftPage/Spells.tsx
+++ src/LeftPage/Spells.tsx
@@ -3,12 +3,33 @@ function Spells() {
         <div>
             <input type="text" placeholder="Search.."></input>
             <hr />
-            <h2><span className="font-bold">Spells:</span> </h2>
+            <h2><span className="font-bold">Spell:</span> </h2>
+            <h1 className="text-2xl font-bold">Burning Hands</h1>
+            <h2 className="italic">1st-level evocation</h2>
+            <h2><span className="font-bold">Casting Time: </span>
+                1 action</h2>
+            <h2><span className="font-bold">Range: </span>
+                Self (15-foot cone)</h2>
+            <h2><span className="font-bold">Components: </span>
+                V, S</h2>
+            <h2><span className="font-bold">Duration: </span>
+                Instantaneous</h2>
+            <br />
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                As you hold your hands with thumbs touching and fingers
+                spread, a thin sheet of flames shoots forth from your outstretched fingertips. Each creature in a 15-foot cone must
+                make a Dexterity saving throw. A creature takes 3d6 fire
+                damage on a failed save, or half as much damage on a
+                successful one.
+                The fire ignites any flammable objects in the area that
+                aren't being worn or carried.
             </p>
+            <br />
+            <h2><span className="font-bold">At Higher Levels. </span>
+                When you cast this spell using a
+                spell slot of 2nd level or higher, the damage increases by
+                1d6 for each slot level above 1st.
+            </h2>
             <hr />
         </div>
     )
